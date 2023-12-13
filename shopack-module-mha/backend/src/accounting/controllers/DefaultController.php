be5042ad3098e158c2c368c70589e1e8eb1e849b
kambizzandi <kambizzandi@gmail.com>
2023-12-13 16:48:56 +0330

    working on basket

--- shopack-module-mha/backend/src/accounting/controllers/DefaultController.php
+++ shopack-module-mha/backend/src/accounting/controllers/DefaultController.php
@@ -9,4 +9,5 @@ use shopack\base\backend\accounting\controllers\BaseAccountingController;
 
 class DefaultController extends BaseAccountingController
 {
+	public $basketModelClass = \iranhmusic\shopack\mha\backend\accounting\models\BasketModel::class;
 }
---
+++ shopack-module-mha/backend/src/accounting/models/BasketModel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Kambiz Zandi <kambizzandi@gmail.com>
+ */
+
+namespace iranhmusic\shopack\mha\backend\accounting\models;
+
+use Yii;
+use shopack\base\backend\accounting\models\BaseBasketModel;
+
+class BasketModel extends BaseBasketModel
+{
+	public $unitModelClass = UnitModel::class;
+	public $productModelClass = ProductModel::class;
+	public $saleableModelClass = SaleableModel::class;
+	public $userAssetModelClass = UserAssetModel::class;
+}
