ce8d5471ce02b31e403a3df82f498af6f5779577
kambizzandi <kambizzandi@gmail.com>
2024-05-04 21:13:00 +0330

    working on 2fa

--- userpanel/assets/AssetManager.php
+++ userpanel/assets/AssetManager.php
@@ -16,19 +16,19 @@ class AssetManager extends \yii\web\AssetManager
 
 	public function init()
 	{
-		$hashCallback = function($path) : string {
-			$path = (is_file($path) ? dirname($path) : $path) . filemtime($path);
-			return sprintf('%x', crc32($path . Yii::getVersion() . '|' . $this->linkAssets));
-		};
+		// $hashCallback = function($path) : string {
+		// 	$path = (is_file($path) ? dirname($path) : $path) . filemtime($path);
+		// 	return sprintf('%x', crc32($path . Yii::getVersion() . '|' . $this->linkAssets));
+		// };
 
-		$this->hashCallback = $hashCallback;
+		// $this->hashCallback = $hashCallback;
 
 		$bower_bootstrap = realpath(Yii::getAlias('@bower/bootstrap'));
-		$vendor_shopack_base_fe = realpath(Yii::getAlias('@vendor/shopack/yii2-base-frontend/src/assets'));
+		$vendor_shopack_base_fe = realpath(Yii::getAlias('@vendor/shopack/yii2-base-frontend/src/common/assets'));
 
 		$this->assetMap = [
-			'bootstrap.css' => $hashCallback($bower_bootstrap) . '/dist/css/bootstrap.rtl.css',
-			'bootstrap-dialog-bs4.css' => $hashCallback($vendor_shopack_base_fe) . '/css/bootstrap-dialog-bs4.rtl.css',
+			'bootstrap.css' => $this->hash($bower_bootstrap) . '/dist/css/bootstrap.rtl.css',
+			'bootstrap-dialog-bs4.css' => $this->hash($vendor_shopack_base_fe) . '/css/bootstrap-dialog-bs4.rtl.css',
 		];
 
 		parent::init();
