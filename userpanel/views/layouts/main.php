83266c9fc064b8cc55fa37f61fda26109d9a969f
kambizzandi <kambizzandi@gmail.com>
2024-07-04 10:30:14 +0330

    working on user

--- userpanel/views/layouts/main.php
+++ userpanel/views/layouts/main.php
@@ -261,7 +261,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => '/f
   <footer id="footer" class="footer mt-auto py-3 bg-light">
     <div class="container">
       <div class="row text-muted">
-        <div class="col text-end">اتوماسیون خانه موسیقی ایران - نسخه 3.1</div>
+        <div class="col text-end">اتوماسیون خانه موسیقی ایران - نسخه 3.2</div>
       </div>
     </div>
   </footer>
