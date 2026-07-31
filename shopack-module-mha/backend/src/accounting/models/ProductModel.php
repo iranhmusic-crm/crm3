2eaa735c8b612c461069f7109aec9f5c143ba0f4
kambiz zandi <kambizzandi@gmail.com>
2026-07-31 18:42:40 +0330

    fix undelete -> make urlparams for calling api

--- shopack-module-mha/backend/src/accounting/models/ProductModel.php
+++ shopack-module-mha/backend/src/accounting/models/ProductModel.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @author Kambiz Zandi <kambizzandi@gmail.com>
  */
@@ -7,27 +8,36 @@ namespace iranhmusic\shopack\mha\backend\accounting\models;
 
 use Yii;
 use iranhmusic\shopack\mha\backend\classes\MhaActiveRecord;
+use shopack\interface\accounting\common\enums\enuProductStatus;
 
 class ProductModel extends MhaActiveRecord
 {
-	use \iranhmusic\shopack\mha\common\accounting\models\ProductModelTrait;
+    use \iranhmusic\shopack\mha\common\accounting\models\ProductModelTrait;
 
-	public static function tableName()
-	{
-		return '{{%MHA_Accounting_Product}}';
-	}
+    use \shopack\base\common\db\SoftDeleteActiveRecordTrait;
+    public function initSoftDelete()
+    {
+        $this->softdelete_RemovedStatus  = enuProductStatus::Removed;
+        // $this->softdelete_StatusField    = 'prdStatus';
+        $this->softdelete_RemovedAtField = 'prdRemovedAt';
+        $this->softdelete_RemovedByField = 'prdRemovedBy';
+    }
 
-	public function behaviors()
-	{
-		return [
-			[
-				'class' => \shopack\base\common\behaviors\RowDatesAttributesBehavior::class,
-				'createdAtAttribute' => 'prdCreatedAt',
-				'createdByAttribute' => 'prdCreatedBy',
-				'updatedAtAttribute' => 'prdUpdatedAt',
-				'updatedByAttribute' => 'prdUpdatedBy',
-			],
-		];
-	}
+    public static function tableName()
+    {
+        return '{{%MHA_Accounting_Product}}';
+    }
 
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => \shopack\base\common\behaviors\RowDatesAttributesBehavior::class,
+                'createdAtAttribute' => 'prdCreatedAt',
+                'createdByAttribute' => 'prdCreatedBy',
+                'updatedAtAttribute' => 'prdUpdatedAt',
+                'updatedByAttribute' => 'prdUpdatedBy',
+            ],
+        ];
+    }
 }
--- shopack-module-mha/backend/src/accounting/models/SaleableModel.php
+++ shopack-module-mha/backend/src/accounting/models/SaleableModel.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @author Kambiz Zandi <kambizzandi@gmail.com>
  */
@@ -8,6 +9,7 @@ namespace iranhmusic\shopack\mha\backend\accounting\models;
 use Yii;
 use yii\web\ServerErrorHttpException;
 use yii\web\UnprocessableEntityHttpException;
+use shopack\interface\accounting\common\enums\enuSaleableStatus;
 use shopack\interface\accounting\common\enums\enuUserAssetStatus;
 use iranhmusic\shopack\mha\backend\classes\MhaActiveRecord;
 use iranhmusic\shopack\mha\common\accounting\enums\enuMhaProductType;
@@ -16,36 +18,45 @@ use iranhmusic\shopack\mha\backend\models\MemberModel;
 
 class SaleableModel extends MhaActiveRecord
 {
-  use \iranhmusic\shopack\mha\common\accounting\models\SaleableModelTrait;
-  use \shopack\interface\accounting\backend\models\BackendSaleableModelTrait;
+    use \iranhmusic\shopack\mha\common\accounting\models\SaleableModelTrait;
+    use \shopack\interface\accounting\backend\models\BackendSaleableModelTrait;
 
-  public static function tableName()
-  {
-    return '{{%MHA_Accounting_Saleable}}';
-  }
+    use \shopack\base\common\db\SoftDeleteActiveRecordTrait;
+    public function initSoftDelete()
+    {
+        $this->softdelete_RemovedStatus  = enuSaleableStatus::Removed;
+        // $this->softdelete_StatusField    = 'slbStatus';
+        $this->softdelete_RemovedAtField = 'slbRemovedAt';
+        $this->softdelete_RemovedByField = 'slbRemovedBy';
+    }
 
-  public function behaviors()
-	{
-		return [
-			[
-				'class' => \shopack\base\common\behaviors\RowDatesAttributesBehavior::class,
-				'createdAtAttribute' => 'slbCreatedAt',
-				'createdByAttribute' => 'slbCreatedBy',
-				'updatedAtAttribute' => 'slbUpdatedAt',
-				'updatedByAttribute' => 'slbUpdatedBy',
-			],
-		];
-	}
+    public static function tableName()
+    {
+        return '{{%MHA_Accounting_Saleable}}';
+    }
 
-  public static function getCustomConditionsToValidDiscountsQuery(
-    $actorID,
-    $validDiscountAlias = 'dscv',
-    $discountWithUsageAlias = 'tmp_dsc_with_usg'
-  ) {
-    $row = null;
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => \shopack\base\common\behaviors\RowDatesAttributesBehavior::class,
+                'createdAtAttribute' => 'slbCreatedAt',
+                'createdByAttribute' => 'slbCreatedBy',
+                'updatedAtAttribute' => 'slbUpdatedAt',
+                'updatedByAttribute' => 'slbUpdatedBy',
+            ],
+        ];
+    }
 
-    if (Yii::$app->user->isGuest == false) { // && Yii::$app->member->isMember) {
-      $qry =<<<SQL
+    public static function getCustomConditionsToValidDiscountsQuery(
+        $actorID,
+        $validDiscountAlias = 'dscv',
+        $discountWithUsageAlias = 'tmp_dsc_with_usg'
+    ) {
+        $row = null;
+
+        if (Yii::$app->user->isGuest == false) { // && Yii::$app->member->isMember) {
+            $qry = <<<SQL
       SELECT  mbrUserID
            ,  GROUP_CONCAT(mbrmgpMemberGroupID) AS grps
            ,  GROUP_CONCAT(mbrknnKanoonID) AS knns
@@ -60,152 +71,153 @@ class SaleableModel extends MhaActiveRecord
        WHERE  mbr.mbrUserID = {$actorID}
 SQL;
 
-      $row = Yii::$app->db->createCommand($qry)->queryOne();
+            $row = Yii::$app->db->createCommand($qry)->queryOne();
+        }
+
+        $qry = '';
+
+        //dscTargetMemberGroupIDs
+        $qry .= " AND  (dscTargetMemberGroupIDs IS NULL";
+        if (empty($row['grps']) == false) {
+            $grps = explode(',', $row['grps']);
+            foreach ($grps as $grp) {
+                $qry .= " OR  JSON_SEARCH(dscTargetMemberGroupIDs, 'one', {$grp}) IS NOT NULL";
+            }
+        }
+        $qry .= ")\n";
+
+        //dscTargetKanoonIDs
+        $qry .= " AND  (dscTargetKanoonIDs IS NULL";
+        if (empty($row['knns']) == false) {
+            $knns = explode(',', $row['knns']);
+            foreach ($knns as $knn) {
+                $qry .= " OR  JSON_SEARCH(dscTargetKanoonIDs, 'one', {$knn}) IS NOT NULL";
+            }
+        }
+        $qry .= ")\n";
+
+        //
+        return $qry;
     }
 
-    $qry = '';
-
-    //dscTargetMemberGroupIDs
-    $qry .= " AND  (dscTargetMemberGroupIDs IS NULL";
-    if (empty($row['grps']) == false) {
-      $grps = explode(',', $row['grps']);
-      foreach ($grps as $grp) {
-        $qry .= " OR  JSON_SEARCH(dscTargetMemberGroupIDs, 'one', {$grp}) IS NOT NULL";
-      }
-    }
-    $qry .= ")\n";
-
-    //dscTargetKanoonIDs
-    $qry .= " AND  (dscTargetKanoonIDs IS NULL";
-    if (empty($row['knns']) == false) {
-      $knns = explode(',', $row['knns']);
-      foreach ($knns as $knn) {
-        $qry .= " OR  JSON_SEARCH(dscTargetKanoonIDs, 'one', {$knn}) IS NOT NULL";
-      }
-    }
-    $qry .= ")\n";
-
-    //
-    return $qry;
-  }
-
-  public static function getCustomConditionsToSaleableWithComputedDiscountsQuery(
-    $actorID,
-    $saleableWithDiscountAlias = 'slbwcv',
-    $productAlias = 'prd'
-  ) {
-    //dscTargetProductMhaTypes
-    $qry =<<<SQL
+    public static function getCustomConditionsToSaleableWithComputedDiscountsQuery(
+        $actorID,
+        $saleableWithDiscountAlias = 'slbwcv',
+        $productAlias = 'prd'
+    ) {
+        //dscTargetProductMhaTypes
+        $qry = <<<SQL
     AND (dscTargetProductMhaTypes IS NULL
      OR JSON_SEARCH(dscTargetProductMhaTypes, 'one', {$productAlias}.prdMhaType) IS NOT NULL
         )
 SQL;
 
-    return $qry;
-  }
+        return $qry;
+    }
 
-  /**
-   * return: new status of item
-   */
-  public static function ProcessVoucherItem($voucherID, $userid, $voucherItemdata)
-  {
-    $orderID = $voucherItemdata['orderID'];
-    $key = $voucherItemdata['key'];
+    /**
+     * return: new status of item
+     */
+    public static function ProcessVoucherItem($voucherID, $userid, $voucherItemdata)
+    {
+        $orderID = $voucherItemdata['orderID'];
+        $key = $voucherItemdata['key'];
 
-    $userAssetModel = UserAssetModel::find()
-      ->innerJoinWith('saleable.product')
-      ->andWhere(['uasID' => $orderID])
-      ->andWhere(['uasUUID' => $key])
-      ->one();
-
-    if ($userAssetModel == null)
-      throw new UnprocessableEntityHttpException("user asset not found");
-
-    if (in_array($userAssetModel->uasStatus, [
-          enuUserAssetStatus::Active, enuUserAssetStatus::Pending
-        ]))
-      return true;
-
-    if ($userAssetModel->uasStatus != enuUserAssetStatus::Draft)
-      return false;
-
-    //start transaction
-    $transaction = Yii::$app->db->beginTransaction();
-
-    try {
-      // if ($userAssetModel->saleable->product->prdMhaType == enuMhaProductType::Membership) {
-      //   $userAssetModel->uasStatus = enuUserAssetStatus::Active;
-
-      // } else if ($userAssetModel->saleable->product->prdMhaType == enuMhaProductType::MembershipCard) {
-
-      //   // wait for card print. after print, status must be changed form Pending to Active
-      //   $userAssetModel->uasStatus = enuUserAssetStatus::Pending;
-
-      // } else {
-      //   throw new UnprocessableEntityHttpException("Invalid mha product type ({$userAssetModel->saleable->product->prdMhaType})");
-      // }
-
-      //discounts
-
-      //id, snid, amount
-      $discounts = [];
-
-      //id => sum(amount)
-      // $discountsAmounts = [];
-
-      $systemDiscounts = $userAssetModel->uasVoucherItemInfo['systemDiscounts'] ?? null;
-      if (empty($systemDiscounts) == false) {
-        foreach ($systemDiscounts as $dk => $dv) {
-          $discounts[] = implode(',', [
-            $userAssetModel->uasActorID,
-            $userAssetModel->uasID,
-            $dv['id'],
-            'NULL',
-            $dv['amount']
-          ]);
-
-          // if (array_key_exists('_' . $dv['id'], $discountsAmounts)) {
-          //   $discountsAmounts['_' . $dv['id']] += $dv['amount'];
-          // } else {
-          //   $discountsAmounts['_' . $dv['id']] = $dv['amount'];
-          // }
-        }
-      }
-
-      $couponDiscount = $userAssetModel->uasVoucherItemInfo['couponDiscount'] ?? null;
-      if (empty($couponDiscount['id']) == false) {
-
-        $discountSerialID = 'NULL';
-        if (empty($couponDiscount['code']) == false) {
-          $discountSerialModel = DiscountSerialModel::find()
-            ->andWhere(['dscsnDiscountID' => $couponDiscount['id']])
-            ->andWhere(['dscsnSN' => $couponDiscount['code']])
+        $userAssetModel = UserAssetModel::find()
+            ->innerJoinWith('saleable.product')
+            ->andWhere(['uasID' => $orderID])
+            ->andWhere(['uasUUID' => $key])
             ->one();
 
-          if ($discountSerialModel != null)
-            $discountSerialID = $discountSerialModel->dscsnID;
-        }
+        if ($userAssetModel == null)
+            throw new UnprocessableEntityHttpException("user asset not found");
 
-        $discounts[] = implode(',', [
-          $userAssetModel->uasActorID,
-          $userAssetModel->uasID,
-          $couponDiscount['id'],
-          $discountSerialID,
-          $couponDiscount['amount']
-        ]);
+        if (in_array($userAssetModel->uasStatus, [
+            enuUserAssetStatus::Active,
+            enuUserAssetStatus::Pending
+        ]))
+            return true;
 
-        // if (array_key_exists('_' . $couponDiscount['id'], $discountsAmounts)) {
-        //   $discountsAmounts['_' . $couponDiscount['id']] += $couponDiscount['amount'];
-        // } else {
-        //   $discountsAmounts['_' . $couponDiscount['id']] = $couponDiscount['amount'];
-        // }
-      }
+        if ($userAssetModel->uasStatus != enuUserAssetStatus::Draft)
+            return false;
 
-      if (empty($discounts) == false) {
-        $discountUsageTableName = DiscountUsageModel::tableName();
-        $discounts = '(' . implode('),(', $discounts) . ')';
+        //start transaction
+        $transaction = Yii::$app->db->beginTransaction();
 
-        $qry =<<<SQL
+        try {
+            // if ($userAssetModel->saleable->product->prdMhaType == enuMhaProductType::Membership) {
+            //   $userAssetModel->uasStatus = enuUserAssetStatus::Active;
+
+            // } else if ($userAssetModel->saleable->product->prdMhaType == enuMhaProductType::MembershipCard) {
+
+            //   // wait for card print. after print, status must be changed form Pending to Active
+            //   $userAssetModel->uasStatus = enuUserAssetStatus::Pending;
+
+            // } else {
+            //   throw new UnprocessableEntityHttpException("Invalid mha product type ({$userAssetModel->saleable->product->prdMhaType})");
+            // }
+
+            //discounts
+
+            //id, snid, amount
+            $discounts = [];
+
+            //id => sum(amount)
+            // $discountsAmounts = [];
+
+            $systemDiscounts = $userAssetModel->uasVoucherItemInfo['systemDiscounts'] ?? null;
+            if (empty($systemDiscounts) == false) {
+                foreach ($systemDiscounts as $dk => $dv) {
+                    $discounts[] = implode(',', [
+                        $userAssetModel->uasActorID,
+                        $userAssetModel->uasID,
+                        $dv['id'],
+                        'NULL',
+                        $dv['amount']
+                    ]);
+
+                    // if (array_key_exists('_' . $dv['id'], $discountsAmounts)) {
+                    //   $discountsAmounts['_' . $dv['id']] += $dv['amount'];
+                    // } else {
+                    //   $discountsAmounts['_' . $dv['id']] = $dv['amount'];
+                    // }
+                }
+            }
+
+            $couponDiscount = $userAssetModel->uasVoucherItemInfo['couponDiscount'] ?? null;
+            if (empty($couponDiscount['id']) == false) {
+
+                $discountSerialID = 'NULL';
+                if (empty($couponDiscount['code']) == false) {
+                    $discountSerialModel = DiscountSerialModel::find()
+                        ->andWhere(['dscsnDiscountID' => $couponDiscount['id']])
+                        ->andWhere(['dscsnSN' => $couponDiscount['code']])
+                        ->one();
+
+                    if ($discountSerialModel != null)
+                        $discountSerialID = $discountSerialModel->dscsnID;
+                }
+
+                $discounts[] = implode(',', [
+                    $userAssetModel->uasActorID,
+                    $userAssetModel->uasID,
+                    $couponDiscount['id'],
+                    $discountSerialID,
+                    $couponDiscount['amount']
+                ]);
+
+                // if (array_key_exists('_' . $couponDiscount['id'], $discountsAmounts)) {
+                //   $discountsAmounts['_' . $couponDiscount['id']] += $couponDiscount['amount'];
+                // } else {
+                //   $discountsAmounts['_' . $couponDiscount['id']] = $couponDiscount['amount'];
+                // }
+            }
+
+            if (empty($discounts) == false) {
+                $discountUsageTableName = DiscountUsageModel::tableName();
+                $discounts = '(' . implode('),(', $discounts) . ')';
+
+                $qry = <<<SQL
 INSERT INTO {$discountUsageTableName} (
       dscusgUserID
     , dscusgUserAssetID
@@ -214,55 +226,54 @@ INSERT INTO {$discountUsageTableName} (
     , dscusgAmount
   ) VALUES {$discounts}
 SQL;
-        $rows = Yii::$app->db->createCommand($qry)->execute();
+                $rows = Yii::$app->db->createCommand($qry)->execute();
 
-        //update tbl_discount moved to trigger
-      }
+                //update tbl_discount moved to trigger
+            }
 
-      //user asset
-      $userAssetModel->uasValidFromDate = $userAssetModel->uasVoucherItemInfo['params']['startDate'] ?? null;
-      $userAssetModel->uasValidToDate = $userAssetModel->uasVoucherItemInfo['params']['endDate'] ?? null;
+            //user asset
+            $userAssetModel->uasValidFromDate = $userAssetModel->uasVoucherItemInfo['params']['startDate'] ?? null;
+            $userAssetModel->uasValidToDate = $userAssetModel->uasVoucherItemInfo['params']['endDate'] ?? null;
 
-      if ($userAssetModel->saleable->product->prdMhaType == enuMhaProductType::Membership) {
-        self::ProcessVoucherItem_Membership($userAssetModel, $voucherItemdata);
-      } else if ($userAssetModel->saleable->product->prdMhaType == enuMhaProductType::MembershipCard) {
-        self::ProcessVoucherItem_MembershipCard($userAssetModel, $voucherItemdata);
-      }
+            if ($userAssetModel->saleable->product->prdMhaType == enuMhaProductType::Membership) {
+                self::ProcessVoucherItem_Membership($userAssetModel, $voucherItemdata);
+            } else if ($userAssetModel->saleable->product->prdMhaType == enuMhaProductType::MembershipCard) {
+                self::ProcessVoucherItem_MembershipCard($userAssetModel, $voucherItemdata);
+            }
 
-      if ($userAssetModel->save() == false)
-        throw new ServerErrorHttpException('It is not possible to create user asset');
+            if ($userAssetModel->save() == false)
+                throw new ServerErrorHttpException('It is not possible to create user asset');
 
-			//commit
-			$transaction->commit();
+            //commit
+            $transaction->commit();
 
-      return true; //enuVoucherItemStatus::Processed; //$userAssetModel->uasStatus;
+            return true; //enuVoucherItemStatus::Processed; //$userAssetModel->uasStatus;
 
-    } catch (\Throwable $th) {
-      //rollback
-      $transaction->rollBack();
-      Yii::error($th, __METHOD__);
-      throw $th;
+        } catch (\Throwable $th) {
+            //rollback
+            $transaction->rollBack();
+            Yii::error($th, __METHOD__);
+            throw $th;
+        }
     }
-  }
 
-  public static function ProcessVoucherItem_Membership(&$userAssetModel, $voucherItemdata)
-  {
-    $memberModel = MemberModel::find()
-      ->andWhere(['mbrUserID' => $userAssetModel->uasActorID])
-      ->one();
-    $memberModel->mbrExpireDate = $userAssetModel->uasValidToDate;
+    public static function ProcessVoucherItem_Membership(&$userAssetModel, $voucherItemdata)
+    {
+        $memberModel = MemberModel::find()
+            ->andWhere(['mbrUserID' => $userAssetModel->uasActorID])
+            ->one();
+        $memberModel->mbrExpireDate = $userAssetModel->uasValidToDate;
 
-    if ($memberModel->save() == false)
-      throw new \Exception(implode(',', $memberModel->getErrorSummary(true)));
+        if ($memberModel->save() == false)
+            throw new \Exception(implode(',', $memberModel->getErrorSummary(true)));
 
-    $userAssetModel->uasStatus = enuUserAssetStatus::Active;
-  }
+        $userAssetModel->uasStatus = enuUserAssetStatus::Active;
+    }
 
-  public static function ProcessVoucherItem_MembershipCard(&$userAssetModel, $voucherItemdata)
-  {
-    //wait for card print.
-    //todo: after print, status must be changed form Pending to Active
-    $userAssetModel->uasStatus = enuUserAssetStatus::Pending;
-  }
-
+    public static function ProcessVoucherItem_MembershipCard(&$userAssetModel, $voucherItemdata)
+    {
+        //wait for card print.
+        //todo: after print, status must be changed form Pending to Active
+        $userAssetModel->uasStatus = enuUserAssetStatus::Pending;
+    }
 }
